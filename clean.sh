d1e6ed790035d7d57d97a55814773780760baa26
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-03 18:56:24 +0200

    Cleaned up

--- clean.sh
+++ clean.sh
@@ -4,11 +4,5 @@ rm Makefile
 rm Makefile.Debug
 rm Makefile.Release
 rm *.pro.user
-rm travis_qmake_gcc_cpp98_boost_test
-rm travis_qmake_gcc_cpp98_boost_test_test
-#rm exe_debug.txt
-#rm exe_release.txt
-#rm test_debug.txt
-#rm test_release.txt
 rm -rf debug
 rm -rf release
